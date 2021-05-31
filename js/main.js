11eb16f4c34c3d7a32a88858d80633c46682360f
milevskiyTG <68344204+milevskiyTG@users.noreply.github.com>
2021-05-31 18:34:30 +0300

    Corrected everything, hope

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 const randomNumber = function (startNumber, endNumber){
-  if (startNumber>=0 && endNumber>=0){
-    if(startNumber<endNumber){
+  if (startNumber >= 0 && endNumber >= 0){
+    if (startNumber < endNumber){
       return  Math.round(startNumber - 0.5 + Math.random() * (endNumber - startNumber + 1)); // Формула взята из интернета
     }
-    else if(startNumber === endNumber){return startNumber;}
-    else{
+    else if (startNumber === endNumber){
+      return startNumber;
+    }else{
       return 'Error: Начало диапазона меньше, чем его конец';
     }
-  }
-  else{
+  }else{
     return 'Error: Диапазон не должен быть отрицательным';
   }
 };
 randomNumber(2, 14);
 
 
-const findByCoordinates = function (startNumber, endNumber, countPoint=0){
-  if (startNumber>=0 && endNumber>=0){
-    if(startNumber<endNumber){
-      return  (startNumber - 0.5 + Math.random() * (endNumber - startNumber + 1)).toFixed(countPoint); // Формула взята из интернета
+const findByCoordinates = function (minCoordinate, maxCoordinate, countPoint = 0){
+  if (startNumber >= 0 && endNumber >= 0){
+    if (startNumber < endNumber){
+      return  (minCoordinate - 0.5 + Math.random() * (maxCoordinate - minCoordinate + 1)).toFixed(countPoint); // Формула взята из интернета
     }
-    else if(startNumber === endNumber){return startNumber;}
-    else{
-      return 'Error: Начало диапазона меньше, чем его конец';
+    else if (minCoordinate === maxCoordinate){
+      return minCoordinate;
+    }else{
+      return 'Error: Диапазон с такими значениями невозможен';
     }
-  }
-  else{
+  }else{
     return 'Error: Диапазон не должен быть отрицательным';
   }
 };
