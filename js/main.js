84cef5d758e2750156c0852f38c47c5c4761a52a
milevskiyTG <68344204+milevskiyTG@users.noreply.github.com>
2021-06-04 11:54:49 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,33 +1,11 @@
-const randomNumber = function (startNumber, endNumber){
-  if (startNumber >= 0 && endNumber >= 0){
-    if (startNumber < endNumber){
-      return  Math.round(startNumber - 0.5 + Math.random() * (endNumber - startNumber + 1)); // Формула взята из интернета
-    }
-    else if (startNumber === endNumber){
-      return startNumber;
-    }else{
-      return 'Error: Начало диапазона меньше, чем его конец';
-    }
-  }else{
-    return 'Error: Диапазон не должен быть отрицательным';
-  }
-};
-randomNumber(2, 14);
-
-
 const findByCoordinates = function (minCoordinate, maxCoordinate, countPoint = 0){
   if (minCoordinate >= 0 && maxCoordinate >= 0){
     if (minCoordinate < maxCoordinate){
-      return  (minCoordinate - 0.5 + Math.random() * (maxCoordinate - minCoordinate + 1)).toFixed(countPoint); // Формула взята из интернета
+      return  (Math.random() * (maxCoordinate - minCoordinate) + minCoordinate).toFixed(countPoint); // Формула взята из интернета
     }
-    else if (minCoordinate === maxCoordinate){
-      return minCoordinate;
-    }else{
-      return 'Error: Диапазон с такими значениями невозможен';
-    }
-  }else{
-    return 'Error: Диапазон не должен быть отрицательным';
+    return (minCoordinate === maxCoordinate) ? minCoordinate : 'Error: Диапазон с такими значениями невозможен';
   }
+  return 'Error: Диапазон не должен быть отрицательным';
 };
-
-findByCoordinates(10, 40, 3);
+console.log(findByCoordinates(1,100));  // Функция для рандомного числа
+console.log(findByCoordinates(1, 100, 2));
